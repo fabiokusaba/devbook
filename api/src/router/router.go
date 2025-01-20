521ec322ee5fdfc342409f22b46ffec31922f06b
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-20 10:08:57 -0300

    adicionado as rotas para dentro do router

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,7 +1,11 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/fabiokusaba/devbook/api/src/router/rotas"
+	"github.com/gorilla/mux"
+)
 
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
