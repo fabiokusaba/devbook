7a87aa3f8527c36d53d05a739ba713e50dfe7c2e
Fabio Kusaba <fabinhokb44@gmail.com>
2025-01-20 11:30:01 -0300

    implementado a funcionalidade de criar usuário

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -28,7 +29,12 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-	repositorio.Criar(usuario)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Usuário com ID: %d inserido com sucesso", usuarioID)))
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
