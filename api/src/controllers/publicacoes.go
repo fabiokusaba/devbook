7df15bada96637885ec53f0ec040765aeb15b27e
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-20 21:54:43 -0300

    implementação da funcionalidade de buscar uma publicação pelo seu id

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -7,8 +7,10 @@ import (
 	"github.com/fabiokusaba/devbook/api/src/modelos"
 	"github.com/fabiokusaba/devbook/api/src/repositorios"
 	"github.com/fabiokusaba/devbook/api/src/respostas"
+	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
@@ -54,7 +56,33 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 	respostas.JSON(w, http.StatusCreated, publicacao)
 }
 
-func BuscarPublicacoes(w http.ResponseWriter, r *http.Request)   {}
-func BuscarPublicacao(w http.ResponseWriter, r *http.Request)    {}
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {}
+
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacao, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacao)
+}
+
 func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {}
-func DeletarPublicacao(w http.ResponseWriter, r *http.Request)   {}
+
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {}
